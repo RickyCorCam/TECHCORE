c74e2c2282ed0003dd553851bbcd91ed2d869b1b
Ricardo Cortes <ricardo.corcam@gmail.com>
2026-05-09 21:09:38 -0600

    ARREGLO

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 
 // URL ORACLE APEX REST API
 // AQUI DESPUES PONDRAS TU URL REAL
-const API_HW = "http://localhost:3000/productos_hw";
-const API_LICENCIAS = "http://localhost:3000/productos_licencias";
-const API_ORDENES = "http://localhost:3000/ordenes";
+const API_HW = "https://tu-api.onrender.com/productos_hw";
+const API_LICENCIAS = "https://tu-api.onrender.com/productos_licencias";
+const API_ORDENES = "https://tu-api.onrender.com/ordenes";
 
 const hardwareContainer = document.getElementById("hardware-container");
 const licenciasContainer = document.getElementById("licencias-container");
